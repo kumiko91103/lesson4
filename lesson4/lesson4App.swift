7c81f5ef17de2f25685b50ec8023278685a7c7ad
sunny <kumiko91103@gmail.com>
2026-03-24 09:32:16 +0800

    Initial Commit

---
+++ lesson4/lesson4App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson4App.swift
+//  lesson4
+//
+//  Created by 114-2Workshop17 on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct lesson4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
